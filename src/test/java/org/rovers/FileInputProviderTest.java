d31ead4bf1264ada2c8446b39849d042af06709d
Jinouga311 <azzeddine.lahmar@zohomail.eu>
2023-09-18 14:58:53 +0200

    Modif

--- src/test/java/org/rovers/FileInputProviderTest.java
+++ src/test/java/org/rovers/FileInputProviderTest.java
@@ -1,5 +1,7 @@
 package org.rovers;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,8 +15,14 @@ class FileInputProviderTest {
 
     private static final String TEST_DATA_DIR = "test_data";
 
-    private static void createTestDataDir() {
+    @BeforeAll
+    static void setUp() {
         new File(TEST_DATA_DIR).mkdirs();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        new File(TEST_DATA_DIR).delete();
     }
 
     private static File createTestFile(String fileName, String content) throws IOException {
@@ -25,7 +33,6 @@ class FileInputProviderTest {
 
     @Test
     void testValidInput() throws IOException {
-        createTestDataDir();
         File file = createTestFile("valid_input.txt", "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n");
         FileInputProvider provider = new FileInputProvider(file.getAbsolutePath());
 
@@ -46,7 +53,6 @@ class FileInputProviderTest {
     @Test
     void testInvalidPlateauDimensions() {
         assertThrows(IllegalArgumentException.class, () -> {
-            createTestDataDir();
             File file = createTestFile("invalid_plateau.txt", "-5 5\n1 2 N\nLMLMLMLMM\n");
             FileInputProvider provider = new FileInputProvider(file.getAbsolutePath());
             provider.getInstructions();
@@ -57,7 +63,6 @@ class FileInputProviderTest {
     @Test
     void testInvalidStartingPosition() {
         assertThrows(IllegalArgumentException.class, () -> {
-            createTestDataDir();
             File file = createTestFile("invalid_start_position.txt", "5 5\n6 2 N\nLMLMLMLMM\n");
             FileInputProvider provider = new FileInputProvider(file.getAbsolutePath());
             provider.getInstructions();
@@ -68,7 +73,6 @@ class FileInputProviderTest {
     @Test
     void testInvalidStartingDirection() {
         assertThrows(IllegalArgumentException.class, () -> {
-            createTestDataDir();
             File file = createTestFile("invalid_start_direction.txt", "5 5\n1 2 X\nLMLMLMLMM\n");
             FileInputProvider provider = new FileInputProvider(file.getAbsolutePath());
             provider.getInstructions();
@@ -79,7 +83,6 @@ class FileInputProviderTest {
     @Test
     void testMissingInstructions() {
         assertThrows(IllegalArgumentException.class, () -> {
-            createTestDataDir();
             File file = createTestFile("missing_instructions.txt", "5 5\n1 2 N\n");
             FileInputProvider provider = new FileInputProvider(file.getAbsolutePath());
             provider.getInstructions();
